17beea05fa36a57941ffbb1006b1fb175bc1d743
Morten Syhler <Morten.syhler@gmail.com>
2020-02-11 15:03:36 +0100

    removed everything

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.semesterproject.two.semester.project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.semesterproject.two.semester.project to javafx.fxml;
-    exports com.semesterproject.two.semester.project;
-}
