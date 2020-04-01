f6f47a6212d309dc916b23f15322072787ea563f
Morten Syhler <32355985+Syhler@users.noreply.github.com>
2020-04-01 12:31:50 +0200

    Change right naming convention

--- src/main/java/org/example/domain/IAuthentication.java
+++ src/main/java/org/example/domain/IAuthentication.java
@@ -2,6 +2,6 @@ package org.example.domain;
 import org.example.entity.*;
 public interface IAuthentication extends  IDomainHandler {
 
-    public UserEntity login(String userName, String passWord);
+    public UserEntity login(String username, String password);
     public Boolean logOut(User user);
 }
