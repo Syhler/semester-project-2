2c619335cad6276ed6675e08ef2140870f18f870
Morten Syhler <Morten.Syhler@gmail.com>
2020-04-03 10:34:12 +0200

    removed unesseary lines

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,15 +3,10 @@ module org.example {
     requires javafx.fxml;
     requires java.sql;
     requires postgresql;
-    //requires jdk.xml.dom;
 
-    //opens org.example.presentation.multipleLanguages to jdk.xml.dom;
     opens org.example to javafx.fxml;
     opens org.example.presentation to javafx.fxml;
 
     exports org.example;
     exports org.example.presentation;
-
-    //exports org.example.presentation.multipleLanguages;
-
 }
