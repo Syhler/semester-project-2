44d28fb255ddd7996d903a329d86977e36d65f8e
LucasAjander <55712282+LucasAjander@users.noreply.github.com>
2020-04-03 12:51:23 +0200

    ProgramUi mega fejl

--- src/main/java/org/example/presentation/DefaultController.java
+++ src/main/java/org/example/presentation/DefaultController.java
@@ -1,17 +1,23 @@
 package org.example.presentation;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
 import org.example.domain.DomainHandler;
+import org.example.domain.Program;
+import org.example.presentation.multipleLanguages.LanguageHandler;
 
-public class DefaultController
+public class DefaultController implements Initializable
 {
 
     public Button login;
+    public Button createProgram;
     private DomainHandler domainHandler = new DomainHandler();
 
     @FXML
@@ -37,6 +43,13 @@ public class DefaultController
         }
     }
 
+    private void createProgram(ActionEvent event)
+    {
+        ProgramController programController = new ProgramController();
+
+    }
+
+
     @FXML
     private void goToSearch() throws IOException {
 
@@ -48,5 +61,10 @@ public class DefaultController
     @FXML
     private void goToUsermanagement() throws IOException {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        createProgram.setText(LanguageHandler.getText("createProgram"));
     }
 }
--- src/main/java/org/example/presentation/ProgramController.java
+++ src/main/java/org/example/presentation/ProgramController.java
@@ -1,11 +1,25 @@
 package org.example.presentation;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.example.App;
 import org.example.domain.DomainHandler;
+import org.example.entity.ProgramEntity;
+import org.example.entity.UserEntity;
 
 public class ProgramController {
     private DomainHandler domainHandler;
+
+
+
+
 
     @FXML
     private void goToCredit() throws IOException {
