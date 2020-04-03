86cbc4f7f0885638fe9b8c52fcb93d646be78d28
Morten Syhler <Morten.Syhler@gmail.com>
2020-04-03 10:32:13 +0200

    finished implemented miltiple language feature

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.presentation.multipleLanguages.Language;
+import org.example.presentation.multipleLanguages.LanguageHandler;
 
 import java.io.IOException;
 
@@ -31,7 +33,9 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        LanguageHandler.initLanguage(Language.Danish);
         launch();
     }
 
