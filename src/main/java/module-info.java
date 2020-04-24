dfb7f7ada7bb599d46113a225d9431b43a33299b
Morten Syhler <Morten.Syhler@gmail.com>
2020-04-24 11:55:00 +0200

    fixed module info missing data

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,16 +6,18 @@ module org.example {
 
 
     opens org.example to javafx.fxml;
-    opens org.example.domain to javafx.base;
+    opens org.example.presentation.login to javafx.fxml;
     opens org.example.presentation to javafx.fxml;
     opens org.example.presentation.program to javafx.fxml;
+    opens org.example.domain.buisnessComponents to javafx.base;
     opens org.example.presentation.usermangement to javafx.fxml;
     opens org.example.presentation.dialogControllers to javafx.fxml;
 
     exports org.example;
-    exports org.example.domain;
     exports org.example.presentation;
+    exports org.example.presentation.login;
     exports org.example.presentation.program;
+    exports org.example.domain.buisnessComponents;
     exports org.example.presentation.usermangement;
     exports org.example.presentation.dialogControllers;
 }
